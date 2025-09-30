5e86de3ca36627f5c2ef194514dbeff25f0f2c06
Ayrus <surya.muppalaneni@gmail.com>
2025-10-01 02:12:15 +0530

    changing path

--- BackEnd/src/index.js
+++ BackEnd/src/index.js
@@ -28,8 +28,8 @@ app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../FrontEnd/dist")));
-  app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../FrontEnd/dist/index.html"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
